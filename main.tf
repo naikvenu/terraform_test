ee89460d98725400fb678cfb7997a2fde1849325
Venu <46160810+naikvenu@users.noreply.github.com>
2019-11-21 14:44:17 +1100

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,11 @@
+provider "oci" {
+  version          = ">= 3.40.0"
+  tenancy_ocid     = "${var.tenancy_id}"
+  user_ocid        = "${var.user_id}"
+  fingerprint      = "${var.fingerprint}"
+  private_key_path = "${var.private_key_path}"
+  region           = "${var.region}"
+}
+
+resource "null_resource "test"{
+}
