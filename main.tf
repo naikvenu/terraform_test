4ec2322d77f279ebebd28d043ee8034814f8227c
Venu <46160810+naikvenu@users.noreply.github.com>
2020-04-16 08:12:50 +1000

    remote back-end etcdv3

--- main.tf
+++ main.tf
@@ -1,10 +1,22 @@
-provider "oci" {
-  tenancy_ocid     = var.tenancy_id
-  user_ocid        = var.user_id
-  fingerprint      = var.fingerprint
-  private_key_path = var.private_key_path
-  region           = var.region
+provider oci {
+  tenancy_ocid     = "${var.tenancy_ocid}"
+  user_ocid        = "${var.user_ocid}"
+  fingerprint      = "${var.fingerprint}"
+  private_key_path = "${var.private_key_path}"
+  region           = "${var.region}"
 }
 
-resource "null_resource" "test" {
+data oci_identity_availability_domains this {
+  compartment_id = "${var.compartment_ocid}"
+}
+
+terraform {
+  backend etcdv3 {
+    endpoints   = ["https://terraform-etcd.tprivatesubnet.terraform.oraclevcn.com:2379"]
+    lock        = true
+    prefix      = "terraform-state-new/"
+    cacert_path = "/home/ubuntu/certs/ca.pem"
+    cert_path   = "/home/ubuntu/certs/terraform.pem"
+    key_path    = "/home/ubuntu/certs/terraform-key.pem"
+  }
 }
