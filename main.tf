f18d0d21fecbc253c6673416e91bf3f9801af21d
Venu <46160810+naikvenu@users.noreply.github.com>
2020-04-15 13:35:36 +1000

    0.12 upgrade

--- main.tf
+++ main.tf
@@ -1,11 +1,10 @@
 provider "oci" {
-  version          = ">= 3.40.0"
-  tenancy_ocid     = "${var.tenancy_id}"
-  user_ocid        = "${var.user_id}"
-  fingerprint      = "${var.fingerprint}"
-  private_key_path = "${var.private_key_path}"
-  region           = "${var.region}"
+  tenancy_ocid     = var.tenancy_id
+  user_ocid        = var.user_id
+  fingerprint      = var.fingerprint
+  private_key_path = var.private_key_path
+  region           = var.region
 }
 
-resource "null_resource "test"{
+resource "null_resource" "test" {
 }
