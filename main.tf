c5e25742100357cfa2bd26906bbaceaba48ee64e
Venu <46160810+naikvenu@users.noreply.github.com>
2020-04-17 15:46:41 +1000

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,13 +1,13 @@
 provider oci {
-  tenancy_ocid     = "${var.tenancy_ocid}"
-  user_ocid        = "${var.user_ocid}"
-  fingerprint      = "${var.fingerprint}"
-  private_key_path = "${var.private_key_path}"
-  region           = "${var.region}"
+  tenancy_ocid     = var.tenancy_id
+  user_ocid        = var.user_id
+  fingerprint      = var.fingerprint
+  private_key_path = var.private_key_path
+  region           = var.region
 }
 
 data oci_identity_availability_domains this {
-  compartment_id = "${var.compartment_ocid}"
+  compartment_id = var.compartment_ocid
 }
 
 terraform {
