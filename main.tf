d914aeb6a0b9454d671cd30adfd7ae4c9d7f3295
Venu <46160810+naikvenu@users.noreply.github.com>
2020-04-17 16:07:46 +1000

    Update main.tf

--- main.tf
+++ main.tf
@@ -20,3 +20,13 @@ terraform {
     key_path    = "/home/ubuntu/certs/terraform-key.pem"
   }
 }
+
+
+variable "tenancy_id" {}
+variable "user_id" {}
+variable "fingerprint" {}
+variable "private_key_path" {}
+variable "region" {}
+variable "compartment_id" {}
+variable "public_key" {}
+variable "private_key" {}
