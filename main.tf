1490d04e62648f509bbca87a164d5acf9b67a413
Venu <46160810+naikvenu@users.noreply.github.com>
2020-04-17 17:48:29 +1000

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider oci {
   user_ocid        = var.user_id
   fingerprint      = var.fingerprint
   private_key_path = var.private_key_path
-  var.private_key_password = var.private_key_password. #Use this only if you have set a passphrase for your private key
+  private_key_password = var.private_key_password. #Use this only if you have set a passphrase for your private key
   region           = var.region
 }
 
@@ -29,5 +29,4 @@ variable "fingerprint" {}
 variable "private_key_path" {}
 variable "region" {}
 variable "compartment_id" {}
-variable "public_key" {}
-variable "private_key" {}
+variable "private_key_password" {}
